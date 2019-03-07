a599334fd2e6e168f3bc56bd9f9494cbfa4a97c1
Rafal Wilinski <raf.wilinski@gmail.com>
2019-03-07 19:54:53 +0100

    Restruct the package

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['packages/*'],
+  displayName: 'aws-lambda-graphql',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '__tests__/.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
